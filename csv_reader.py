9ee49395045392906a15475250a2fa3892d68f73
ituitis20-tuncerb19 <tuncerb19@itu.edu.tr>
2022-12-14 14:32:20 +0300

    csv_reader updated

--- csv_reader.py
+++ csv_reader.py
@@ -40,6 +40,7 @@ for A in players.values:
     player = Player(A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7],A[8],A[9],A[10],A[11])
     myDB.add(player)
 
+
 games = pd.read_csv("csv_files/games.csv")
 print(games.keys())
 #games.drop(columns=[],inplace=True)
@@ -49,15 +50,22 @@ for A in games.head(5).values:
     game = Game(A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7],A[8],A[9],A[10],A[11],A[12],A[13],A[14],A[15],A[16],A[17],A[18],A[19],A[20])
     myDB.add(game)
 
+
+appearances = pd.read_csv("csv_files/appearances.csv")
+print(appearances.keys())
+#appearance.drop(columns=[],inplace=True)
+print(appearances.keys())
+for A in appearances.values:
+    appearance = Appearance(A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7],A[8],A[9],A[10],A[11])
+    myDB.add(appearance)
+
+
 player_valuations = pd.read_csv("csv_files/player_valuations.csv")
 print(player_valuations.keys())
-player_valuations.drop(columns=['url','image_url','country_of_birth','sub_position','name',
-                    'agent_name', 'contract_expiration_date', 'domestic_competition_id',
-                    'club_name', 'last_season'],inplace=True)
+player_valuations.drop(columns=["date"],inplace=True)
 print(player_valuations.keys())
 for A in player_valuations.values:
-    player_valuation = PlayerValuation(A[0],A[1],A[2],A[3],A[4],A[5],A[6],A[7],A[8],A[9],A[10],A[11])
+    player_valuation = PlayerValuation(A[0],A[1],A[2],A[3],A[4],A[5])
     myDB.add(player_valuation)
-
 
 
